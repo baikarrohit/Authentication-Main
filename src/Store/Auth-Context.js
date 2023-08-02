bf69ec865a85da35c506d21c7bee24e5604da8ad
baikarrohit <baikarrohit1999@gmail.com>
2023-08-02 19:06:23 +0530

    Implemented auto logout

--- src/Store/Auth-Context.js
+++ src/Store/Auth-Context.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   token: "",
@@ -12,16 +12,34 @@ export const AuthContextProvider = (props) => {
   const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
+  let logoutTimer;
 
   const loginHandler = (token) => {
     setToken(token);
     localStorage.setItem("token", token);
+    resetLogoutTimer(); // Start the logout timer on login
   };
 
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
+    clearTimeout(logoutTimer); // Clear the logout timer on logout
   };
+
+  // Function to start/reset the logout timer
+  const resetLogoutTimer = () => {
+    clearTimeout(logoutTimer);
+    logoutTimer = setTimeout(() => {
+      logoutHandler();
+    }, 5 * 60 * 1000); // 5 minutes in milliseconds
+  };
+
+  // Listen for token changes to start/reset the logout timer
+  useEffect(() => {
+    if (token) {
+      resetLogoutTimer();
+    }
+  });
 
   const contextValue = {
     token: token,
